6f55981e51277dc24b12aed564e130cb7cf3d8ac
Roger Wells <rsw359@gmail.com>
2023-12-12 09:36:16 +0900

    cleanup

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
-import { users, posts } from "./data/index.js";
 
 /* Configurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -59,8 +58,7 @@ mongoose
 	})
 	.then(() => {
 		app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
-		/* seed data */
-		// User.insertMany(users);
+
 		// Post.insertMany(posts);
 	})
 	.catch((error) => console.log(`${error} did not connect`));
